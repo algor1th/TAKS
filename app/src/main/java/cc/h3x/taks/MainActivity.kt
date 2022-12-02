097d005f947ea106a6c3b54f70261a60feb3b913
Lenny Händler <lenny@lennyhaendler.com>
2022-12-02 21:23:04 +0100

    Layout changes
    * Remove Fragments from main view
      fixes crash on switch to dark mode
    * Use dynamic colors

--- app/src/main/java/cc/h3x/taks/MainActivity.kt
+++ app/src/main/java/cc/h3x/taks/MainActivity.kt
@@ -2,20 +2,15 @@ package cc.h3x.taks
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
 import cc.h3x.taks.databinding.ActivityMainBinding
 import com.google.android.material.color.DynamicColors
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,39 +20,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navController.navigate(R.id.action_FirstFragment_to_SecondFragment)
-
         binding.fab.setOnClickListener { view ->
             startActivity(Intent(this, AddTodo::class.java))
         }
 
-        DynamicColors.applyIfAvailable(this)
-    }
+        val db = Room.databaseBuilder(
+            applicationContext,
+            TodoDatabase::class.java, getString(R.string.taskDbName)
+        ).allowMainThreadQueries().build()
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+        binding.rvTodoItems.layoutManager = LinearLayoutManager(this)
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
+        val adapter = TodoAdapter(db.todoDao())
+        binding.rvTodoItems.adapter = adapter
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        ItemTouchHelper(SwipeItem(adapter)).attachToRecyclerView(binding.rvTodoItems)
+
+        DynamicColors.applyToActivityIfAvailable(this)
     }
 }
